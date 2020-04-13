2a0595fa3525a794a37006216ceca8d733198ef5
smkang <ehfl1230@gmail.com>
2020-04-14 00:34:09 +0900

    5장 완료.

--- src/main/java/com/msgnak/proj/springboot/service/posts/PostsService.java
+++ src/main/java/com/msgnak/proj/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.msgnak.proj.springboot.service.posts;
 
 import com.msgnak.proj.springboot.web.domain.posts.Posts;
 import com.msgnak.proj.springboot.web.domain.posts.PostsRepository;
+import com.msgnak.proj.springboot.web.dto.PostsListResponseDto;
 import com.msgnak.proj.springboot.web.dto.PostsResponseDto;
 import com.msgnak.proj.springboot.web.dto.PostsSaveRequestDto;
 import com.msgnak.proj.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,8 +35,22 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new
-                        IllegalArgumentException("해당 사용자가 없스비다. id=" + id));
+                        IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 }
